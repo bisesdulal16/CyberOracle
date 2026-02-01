9093d12a1c30546f9cce1343147f6557e3ba1f93
Bishesh Dulal <117180644+bisesdulal16@users.noreply.github.com>
2026-02-01 12:09:18 -0600

    Revert "Fix CI workflow for nexus docker registry and ignore local files"

--- populate_compliance_logs.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO logs (created_at, endpoint, method, status_code, message, masked)
-VALUES
-(NOW(), '/api/patient/1', 'POST', 200, 'Processed request successfully', false),
-(NOW(), '/api/patient/2', 'POST', 200, 'Processed request successfully', false),
-(NOW(), '/api/login', 'POST', 401, 'Unauthorized access attempt', false),
-(NOW(), '/api/login', 'POST', 403, 'Forbidden access attempt', false),
-(NOW(), '/api/patient', 'POST', 200, 'SSN=***MASKED***', true);
